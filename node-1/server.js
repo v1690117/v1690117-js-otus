72647cfb78bf534c201d6c6ee2f39f102d2f1918
Vladimir <vveselov@igatec.com>
2018-12-08 19:26:21 +0300

    Исправлены замечания. Скрипт теперь не падает при запуске без аргументов. Добавлено описание. Часть кода вынесена в константы. Основной код в скрипте request обернут в функцию run(). Исправлено оформление комментариев.

--- node-1/server.js
+++ node-1/server.js
@@ -1,28 +1,22 @@
-`
-Реализовать скрипт request для тестирования веб сервера
-Создать локальный веб сервер 'server', отвечающий на запросы каждые 100ms
+/**
+ * Создать локальный веб сервер 'server', отвечающий на запросы каждые 100ms
+ */
 
-Создать скрипт 'request', принимающий на вход 
-- количество запросов 'N'
-- тип запросов - параллельный или последовательный
-
-Скрипт 'request' должен отправлять 'N' последовательных или параллельных 'HTTP' 
-запросов к локальному серверу 'server'
-`
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const http = require('http')
+const HOST = '127.0.0.1'
+const PORT = 3000
+const TIMEOUT = 100
+const STATUS_OK = 200
 
 let i = 0
 const server = http.createServer((req, res) => {
-    setTimeout(()=>{
+    setTimeout(() => {
         console.log(++i)
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('');
-    }, 100)
+        res.statusCode = STATUS_OK
+        res.end()
+    }, TIMEOUT)
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(PORT, HOST, () => {
+    console.log(`Server running at http://${HOST}:${PORT}/`)
 });
